d1a6ec4ae1aa3b3dc32e4f8bbfd3cbd709b0c589
madorik <madorik88@gmail.com>
2020-09-21 18:08:00 +0900

    commit : use logback

---
+++ src/test/java/com/estsoft/pilot/LogbackTest.java
@@ -0,0 +1,23 @@
+package com.estsoft.pilot;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+/**
+ * Create by madorik on 2020-09-21
+ */
+@SpringBootTest
+public class LogbackTest {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Test
+    void log() {
+        logger.info("[INFO]");
+        logger.debug("[DEBUG]");
+        logger.warn("[WARN]");
+        logger.error("[ERROR]");
+    }
+}
