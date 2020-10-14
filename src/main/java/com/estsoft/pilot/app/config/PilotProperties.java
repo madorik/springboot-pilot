154f29f02106cdfde5f6b6ebcfc84212a8a2b363
madorik <madorik88@gmail.com>
2020-10-14 18:12:20 +0900

    commit : 추후 설정 파일을 외부 연동(disk or spring cloud config)

--- src/main/java/com/estsoft/pilot/app/config/PilotProperties.java
+++ src/main/java/com/estsoft/pilot/app/config/PilotProperties.java
@@ -3,12 +3,15 @@ package com.estsoft.pilot.app.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Create by madorik on 2020-09-28
  */
 @Getter
 @Setter
+@Configuration
+//@PropertySources(@PropertySource(value = "file:D:\\application.properties"))
 @ConfigurationProperties(prefix = "app", ignoreInvalidFields = true)
 public class PilotProperties {
 
